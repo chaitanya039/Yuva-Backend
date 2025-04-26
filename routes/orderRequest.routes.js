a7e049206dbc8ad79cea9f4379da280f453b2f20
Chaitanya Pansare <chaitanyapansare33607@gmail.com>
2025-04-26 19:40:03 +0530

    Deployment!

--- routes/orderRequest.routes.js
+++ routes/orderRequest.routes.js
@@ -7,16 +7,16 @@ import {
   getOrderRequestsByCustomer
 } from '../controllers/orderRequest.controller.js';
 
-import { protectUser } from '../middlewares/auth.middleware.js';
+import { protectCustomer, protectUser } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 // Customer submits request
-router.post('/', protectUser, createOrderRequest);
+router.post('/', protectCustomer, createOrderRequest);
 
 // Admin manages requests
 router.get('/', protectUser, getAllOrderRequests);
-router.get("/my", protectUser, getOrderRequestsByCustomer);
+router.get("/my", protectCustomer, getOrderRequestsByCustomer);
 router.put('/approve/:id', protectUser, approveOrderRequest);
 router.put('/reject/:id', protectUser, rejectOrderRequest);
 
